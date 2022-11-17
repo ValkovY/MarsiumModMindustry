8724190a317f5b0cc8f881405cbd03b866a0e3e5
Ilya <68284331+ValkovY@users.noreply.github.com>
2022-11-17 17:56:22 +0300

    Update VMPlanets.java

--- src/vm/content/VMPlanets.java
+++ src/vm/content/VMPlanets.java
@@ -18,29 +18,50 @@ public class VMPlanets{
     public static Planet marsium;
 
     public static void load() {
-        marsium = new Planet("marsium", Planets.sun, 1f, 3) {{
+        marsium = new Planet("marsium", Planets.sun, 1f, 2) {{
             generator = new ErekirPlanetGenerator();
             hasAtmosphere = true;
             meshLoader = () -> new MultiMesh(
-                    new HexMesh(this, 4)
+                    new HexMesh(this, 6)
+            );
+             cloudMeshLoader = () -> new MultiMesh(
+                new HexSkyMesh(this, 1, 0.15f, 0.14f, 5, Color.valueOf("eba768").a(0.75f), 2, 0.42f, 1f, 0.43f),
+                new HexSkyMesh(this, 2, 0.6f, 0.15f, 5, Color.valueOf("d95030").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
             );
             allowSectorInvasion = false;
-            atmosphereColor = Color.valueOf("ffb02e");
+            atmosphereColor = Color.valueOf("#f4a460");
             atmosphereRadIn = 0.02f;
             atmosphereRadOut = 0.3f;
-            landCloudColor = Color.valueOf("#9f8170");
-            orbitRadius = 60f;
+            landCloudColor = Color.valueOf("#d95030");
+            orbitRadius = 70f;
             startSector = 3;
             accessible = true;
             alwaysUnlocked = true;
             bloom = false;
             orbitTime = Mathf.pow(orbitRadius, 1.5f) * 960;
+            iconColor = Color.valueOf("#c51d34");
             ruleSetter = r -> {
               r.waveTeam = Team.blue;
               r.attributes.set(Attribute.heat, -0.2f);
               r.showSpawns = true;
               r.coreCapture = true;
               r.coreIncinerates = false;
+            };
+        }};
+        extinction = new Planet("extinction", marsium, 1f, 0) {{
+            hasAtmosphere = false;
+            meshLoader = () -> new MultiMesh(
+                    new HexMesh(this, 5)
+            );
+            radius = 1f;
+            allowSectorInvasion = false;
+            startSector = 3;
+            accessible = true;
+            alwaysUnlocked = true;
+            bloom = false;
+            orbitTime = Mathf.pow(orbitRadius, 1.5f) * 960;
+            iconColor = Color.valueOf("#464451");
+            visible = true;
             };
         }};
     }
