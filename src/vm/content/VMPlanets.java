fdaaa263051aee2a65c9af87601a8343553bb5ea
Ilya <68284331+ValkovY@users.noreply.github.com>
2022-11-17 16:31:49 +0300

    Create VMPlanets.java

---
+++ src/vm/content/VMPlanets.java
@@ -0,0 +1,43 @@
+package vm.content;
+
+import arc.graphics.Color;
+import arc.math.Mathf;
+import mindustry.game.Team;
+import mindustry.graphics.g3d.*;
+import mindustry.type.Planet;
+import mindustry.world.meta.Attribute;
+
+public class VMPlanets{
+
+    public static Planet marsium;
+
+    public static void load() {
+        marsium = new VMPlanet("marsium", sun, 1f, 3) {{
+            averageSurfaceTemperature = 200f;
+            orbitRad = 446;
+            generator = new ErekirPlanetGenerator();
+            hasAtmosphere = true;
+            meshLoader = () -> new MultiMesh(
+                    new HexMesh(this, 4)
+            );
+            allowSectorInvasion = false;
+            atmosphereColor = Color.valueOf("ffb02e");
+            atmosphereRadIn = 0.02f;
+            atmosphereRadOut = 0.3f;
+            landCloudColor = Color.valueOf("#9f8170	");
+            orbitRadius = 60f;
+            startSector = 3;
+            accessible = true;
+            alwaysUnlocked = true;
+            bloom = false;
+            orbitTime = Mathf.pow(orbitRadius, 1.5f) * 960;
+            ruleSetter = r -> {
+              r.waveTeam = Team.blue;
+              r.attributes.set(Attribute.heat, -0.2f);
+              r.showSpawns = true;
+              r.coreCapture = true;
+              r.coreIncinerates = false;
+            };
+        }};
+    }
+}
