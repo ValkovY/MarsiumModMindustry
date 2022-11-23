2b81746e4713f7c52c8f39c7b105494231ad5574
Ilya <68284331+ValkovY@users.noreply.github.com>
2022-11-23 17:44:16 +0300

    Update VMItems.java

--- src/vm/content/VMItems.java
+++ src/vm/content/VMItems.java
@@ -8,12 +8,20 @@ public class VMItems {
 		niobium;
 
     public static void load() {
-        niobium = new Item("niobium", Color.valueOf("abcdef")) {{
+        niobium = new Item("niobium", Color.valueOf("879aa1")) {{
             radioactivity = 0.70f;
             cost = 2;
             charge = 0.04f;
             hardness = 5;
             flammability = 0.01f;
+        }};
+	    
+        redGround = new Item("red-ground", Color.valueOf("f4a460")) {{
+            radioactivity = 0f;
+            cost = 2;
+            charge = 0f;
+            hardness = 1;
+            flammability = 0f;
         }};
     }
 }
