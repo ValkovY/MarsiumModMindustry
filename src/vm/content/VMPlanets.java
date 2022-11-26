9655546a9f38f624418d979104995be788ba738d
Ilya <68284331+ValkovY@users.noreply.github.com>
2022-11-26 23:08:34 +0300

    Update VMPlanets.java

--- src/vm/content/VMPlanets.java
+++ src/vm/content/VMPlanets.java
@@ -21,9 +21,9 @@ public class VMPlanets{
         marsium = new Planet("marsium", Planets.sun, 1f, 2) {{
             generator = new ErekirPlanetGenerator();
             hasAtmosphere = true;
-            meshLoader = () -> new MultiMesh(
+            //*meshLoader = () -> new MultiMesh(
                     new HexMesh(this, 6)
-            );
+            //*);
              cloudMeshLoader = () -> new MultiMesh(
                 new HexSkyMesh(this, 1, 0.15f, 0.14f, 5, Color.valueOf("#BC987E").a(0.75f), 2, 0.42f, 1f, 0.43f),
                 new HexSkyMesh(this, 2, 0.6f, 0.15f, 5, Color.valueOf("#EA7E5D").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
