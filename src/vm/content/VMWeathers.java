b8748c8f797f102f8e9ea985c45e032767253ac2
Ilya <68284331+ValkovY@users.noreply.github.com>
2022-12-08 17:24:20 +0300

    Create VMWeathers.java

---
+++ src/vm/content/VMWeathers.java
@@ -0,0 +1,37 @@
+package vm.content;
+
+import arc.graphics.*;
+import arc.util.*;
+import mindustry.gen.*;
+import mindustry.type.*;
+import mindustry.type.weather.*;
+import mindustry.world.meta.*;
+
+public class Weathers{
+    public static Weather
+    redSandstorm;
+
+    public static void load(){
+        redSandstorm = new ParticleWeather("redSandstorm"){{
+            pcolor = Color.valueOf("F4A560");
+            noiseColor = Color.valueOf("CF8B53");
+            particleRegion = "particle";
+            drawNoise = true;
+            useWindVector = true;
+            sizeMax = 150f;
+            sizeMin = 50f;
+            minAlpha = 0f;
+            maxAlpha = 0.2f;
+            density = 2000f;
+            baseSpeed = 7f;
+            attrs.set(Attribute.light, -0.5f);
+            attrs.set(Attribute.water, -0.5f);
+            opacityMultiplier = 0.35f;
+            force = 0.5f;
+            sound = Sounds.wind;
+            soundVol = 0.8f;
+            duration = 7f * Time.toMinutes;
+            statusGround = false;
+        }};
+    }
+}
